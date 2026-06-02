870725c2a27a17fb8911ab835a61d098a62e45a8
Lavanya-Asuti <lavuasuti22@gmail.com>
2026-06-02 11:10:18 +0530

    Replace PostgreSQL load with CSV

--- etl/load.py
+++ etl/load.py
@@ -1,14 +1,3 @@
-from sqlalchemy import create_engine
-
 def load(df):
     df.to_csv("output.csv", index=False)
     print("Data loaded successfully")
-
-    df.to_sql(
-        "transaction",
-        engine,
-        if_exists="replace",
-        index=False
-    )
-
-    print("Loaded into SQL")
